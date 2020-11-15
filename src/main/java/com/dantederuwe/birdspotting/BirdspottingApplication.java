238e3efb1bc0a816adb7f4365a5389a037155072
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-11-15 16:06:39 +0100

    Initial commit

---
+++ src/main/java/com/dantederuwe/birdspotting/BirdspottingApplication.java
@@ -0,0 +1,13 @@
+package com.dantederuwe.birdspotting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdspottingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdspottingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dantederuwe/birdspotting/BirdspottingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantederuwe.birdspotting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdspottingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
