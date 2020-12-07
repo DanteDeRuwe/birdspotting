9ffb1e851484baddfaef64b5d551d02581cbcabf
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-07 12:05:44 +0100

    Enable basic security

---
+++ src/main/java/com/dantederuwe/birdspotting/security/SecurityConfigurer.java
@@ -0,0 +1,34 @@
+package com.dantederuwe.birdspotting.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().defaultSuccessUrl("/birdspotting",true);
+
+        http.authorizeRequests()
+                .antMatchers("/*").hasAnyRole("SPOTTER", "ADMIN")
+                .antMatchers("/**/create-new-spotting").hasRole("ADMIN")
+                .and().csrf();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("spotter")
+                .password("{noop}RedDuck007")
+                .roles("SPOTTER")
+                .and()
+                .withUser("admin")
+                .password("{noop}eagle")
+                .roles("ADMIN");
+    }
+}
